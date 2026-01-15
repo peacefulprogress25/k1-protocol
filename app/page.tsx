9ac7206c9d36c4adc045d88378a8fbb2f774b9a2
Utkarsh Patel <70640377+peacefulprogress25@users.noreply.github.com>
2026-01-15 14:29:13 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
 
         {/* BACKGROUND IMAGE */}
         <img
-          src="https://i.ibb.co/5gGRR4hC/Chat-GPT-Image-Jan-13-2026-05-12-32-PM.png"
+          src="https://i.ibb.co/PyyMJQP/Gemini-Generated-Image-xe025ixe025ixe02.png"
           alt="K1 energy future"
           className="absolute inset-0 w-full h-full object-cover"
         />
