04b6f38bc8d591ddad944422e1d1e84848fecf33
Utkarsh Patel <70640377+peacefulprogress25@users.noreply.github.com>
2026-01-20 16:10:49 +0530

    Update navigation.tsx

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -7,7 +7,7 @@ export function Navigation() {
       <div className="mx-auto flex items-center justify-between px-6 py-4 lg:px-12">
         {/* Left: K1 Logo */}
         <a href="/" className="flex items-center gap-2">
-  <img src="/k1-logo.svg" alt="K1" className="h-6 w-6" />
+  <img src="/k1-logo.svg" alt="K1" className="h-10 w-auto" />
 </a>
 
 
